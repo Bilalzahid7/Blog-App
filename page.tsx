2d309a1f3823d774088c9980a7899813e8499989
Bilalzahid7 <122958963+Bilalzahid7@users.noreply.github.com>
2024-01-05 20:26:36 +0500

    Add files via upload
    page.tsx code for dynamic routing

--- page.tsx
+++ page.tsx
@@ -1,40 +1,38 @@
-import Card from "../card";
-export async function getBlogs() {
-  const res = await fetch(
-    "https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=10"
-  );
+import { getBlogs } from "../page";
 
-  if (!res.ok) {
-    throw new Error("Could not find anything ");
-  }
-
-  return await res.json();
-}
-
-async function Blogs() {
+async function BlogId({
+  params,
+}: {
+  params: {
+    blogId: string;
+  };
+}) {
   const blogs = await getBlogs();
+  const filterData = blogs.blogs.find((elem: any) => elem.id == params.blogId);
 
   return (
     <>
-      <h1 className="text-3xl font-bold m-8">
-        WEEKLY ARTICLES WITH INSIGHT INTO THE WEEKEND'S MESSAGE
+      <h1 className="text-center text-3xl font-bold mt-8 mb-4">
+        Welcome To the Detail Page
       </h1>
-      <h2 className="text-2xl m-8">
-        Our blog takes the message from the weekend and lays out next right
-        steps, so you can hear a message and do a message in practical ways.
-      </h2>
-      <div className="p-10 flex gap-5 flex-wrap justify-center">
-        {blogs.blogs.map((elem: any) => (
-          <Card
-            iUrl={elem.photo_url}
-            title={elem.title}
-            description={elem.description}
-            create={elem.created_at}
-            id={elem.id}
-          />
-        ))}
+
+      <div className="max-w-screen-2xl mx-auto bg-white p-8 rounded-md shadow-md">
+        <img
+          src={filterData.photo_url}
+          alt="Blog Post Image"
+          className="w-full h-80 object-cover mb-4 rounded-md"
+        />
+
+        <h1 className="text-3xl font-bold mb-2">{filterData.title}</h1>
+
+        <p className="text-gray-600 mb-4 text-3xl font-bold ">
+          {filterData.description}
+        </p>
+
+        <div className="prose text-justify">{filterData.content_text}</div>
       </div>
     </>
   );
 }
-export default Blogs;
+
+export default BlogId;
